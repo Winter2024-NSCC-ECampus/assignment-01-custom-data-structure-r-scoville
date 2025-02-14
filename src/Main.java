c4d962993d6500df2fc8c49ac5d229ee031f9ea5
r-scoville <rosemsco@live.com>
2025-02-13 22:28:55 -0400

    Completed all items. TODO: Format the output for nicer reading, replace hardcoded arguments with user input for fun.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import custom_linkedlist.LinkedList;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -11,42 +13,59 @@ public class Main {
         // ll.tail();
 
         // 1. Append a new node to the end of the list.
+        System.out.println("1. APPEND:");
         ll.head = ll.append(2);
         ll.printLL();
 
         // 2. Prepend a new node to the start of the list.
+        System.out.println("2. PREPEND:");
         ll.head = ll.prepend(1);
         ll.printLL();
 
         // Test the append() method again, now that the linked list is populated.
-         ll.append(33);
-         ll.append(400);
-         ll.printLL();
+        System.out.println("APPEND (TEST 2):");
+        ll.append(33);
+        ll.append(400);
+        ll.printLL();
 
         // 3. Size: return the total number of nodes in the list.
+        System.out.println("3. SIZE:");
         ll.size();
         System.out.println("Total number of nodes: " + ll.size() + "\n");
 
         // 4. Head: return the first node in the list (both the address and the value).
+        System.out.println("4. HEAD:");
         ll.head();
 
         // 5. Tail: return the last node in the list.
+        System.out.println("5. TAIL:");
         ll.tail();
 
         // 6. At: return the node at the given index.
-        ll.at(0);
-        ll.at(1);
-        ll.at(2);
-        ll.at(3);
-        ll.at(4);
+        System.out.println("6. AT:");
+        ll.at(0); // Test to return the node at a valid index.
+        ll.at(1); // "
+        ll.at(2); // "
+        ll.at(3); // "
+        ll.at(4); // Test to return out-of-bounds.
+        System.out.println("");
 
         // 7. Pop: remove the last element from the list.
+        System.out.println("7. POP:");
         ll.pop();
         ll.pop();
 
         // 8. Contains?: return true if the passed-in value is in the list. Otherwise, return false.
+        System.out.println("8. CONTAINS:");
+        ll.contains(1); // Test to return true.
+        ll.contains(2); // Test to return true.
+        ll.contains(100); // Test to return false.
 
         // 9. Find: return the index of the node containing the value, or nil if not found.
+        System.out.println("9. FIND:");
+        ll.find(1); // Test to return a valid index.
+        ll.find(2); // Test to return a valid index.
+        ll.find(200); // Test to return a value not found.
 
     }
 }
--- src/custom_linkedlist/LinkedList.java
+++ src/custom_linkedlist/LinkedList.java
@@ -3,45 +3,43 @@ package custom_linkedlist;
 public class LinkedList {
 
     public Node head;
-    public Node tail;
     public int size = size();
+
 
     // --------------------------------------------------------------------------
     // 1. append(value) adds a new node containing value to the end of the list.
-    // TODO: Refactor append() to take in only value.
 
-    public Node append(int data) {
+    public Node append(int value) {
 
-        Node new_node = new Node(data); // Create a new node.
+        Node newNode = new Node(value); // Create a new node.
+        Node currentNode = head;
 
         if (head == null) { // Check if the linked list is empty.
-            head = new_node; // If empty, make the new node the head.
-            tail = head;
+            head = newNode; // If empty, make the new node the head.
         } else {
-            tail = head;
-            while (tail.next != null) { // If the linked list is not empty, go to end of the list.
-                tail = tail.next; // Reassign the current tail.
+            while (currentNode.next != null) { // If the linked list is not empty, go to end of the list.
+                currentNode = currentNode.next; // Reassign the current tail.
             }
-            tail.next = new_node; // Set the current tail's next to point to the new node.
-            tail = tail.next; // Insert the new node at the tail.
+            currentNode.next = newNode; // Set the current tail's next to point to the new node.
         }
-        System.out.println("New node with a value of '" + data + "' successfully appended to linked list.");
+        System.out.println("New node with a value of '" + value + "' successfully appended to linked list.");
         return head;
     }
+
 
     // --------------------------------------------------------------------------
     // 2. prepend(value) adds a new node containing value to the start of the list.
-    // TODO: Refactor prepend() to take in only value.
 
-    public Node prepend(int data) {
+    public Node prepend(int value) {
 
-        Node newNode = new Node(data); // Create a new node
+        Node newNode = new Node(value); // Create a new node
 
         newNode.next = head; // If the linked list is not empty, make the current head the next node.
         head = newNode; // Insert the new node at the head.
-        System.out.println("New node with a value of '" + data + "' successfully prepended to linked list.");
+        System.out.println("New node with a value of '" + value + "' successfully prepended to linked list.");
         return head;
     }
+
 
     // --------------------------------------------------------------------------
     // 3. size returns the total number of nodes in the list.
@@ -58,6 +56,7 @@ public class LinkedList {
         return count;
     }
 
+
     // --------------------------------------------------------------------------
     // 4. head returns the first node in the list.
 
@@ -71,24 +70,27 @@ public class LinkedList {
         return head; // Return the first node in the list.
     }
 
+
     // --------------------------------------------------------------------------
     // 5. tail returns the last node in the list.
 
     public Node tail() {
-        if (tail != null) { // Check for an empty list.
-            System.out.println("The last node's address: " + tail);
-            System.out.println("The last node's value: " + tail.value + "\n");
+        Node currentNode = head;
+
+        if (currentNode != null) { // Check for an empty list.
+            System.out.println("The last node's address: " + currentNode);
+            System.out.println("The last node's value: " + currentNode.value + "\n");
         } else {
             System.out.println("This list is empty. There is no tail node. \n");
         }
-        return tail; // Return the last node in the list.
+        return currentNode; // Return the last node in the list.
     }
+
 
     // --------------------------------------------------------------------------
     // 6. at(index) returns the node at the given index.
 
     public Node at(int index) {
-
         Node currentNode = head; // Create a node to hold the value to iterate and return.
         int counter = 0;
         int indexRange = size() - 1; // Create a variable to hold the searchable index range.
@@ -109,6 +111,7 @@ public class LinkedList {
         return currentNode; // Return the node at the given index if it exists.
     }
 
+
     // --------------------------------------------------------------------------
     // 7. pop removes the last element from the list.
     public Node pop() {
@@ -123,7 +126,7 @@ public class LinkedList {
             }
             prevNode.next = null; // Set the second-last node to be the new tail.
 
-            System.out.println("Popped node:");
+            System.out.println("\nPopped node:");
             System.out.println("- Value: " + currentNode.value);
             System.out.println("- Address: " + currentNode);
             System.out.println("Updated linked list:");
@@ -137,13 +140,62 @@ public class LinkedList {
         }
     }
 
+
     // -------------------------------------------------------------------------------------------------
     // 8. contains?(value) returns true if the passed in value is in the list and otherwise returns false.
-    // TODO
+    public boolean contains(int value) {
+
+        boolean containsValue = false; // Create variable to hold the return.
+        Node currentNode = head; // Create variable to hold the current node as the function iterates.
+        int listSize = size();
+
+        // Iterate through the linked list and compare each node value to the value requested.
+        for (int i = 0; i < listSize; i++) {
+            if (currentNode.value == value) {
+                containsValue = true; // If the value is found, return true.
+                break;
+            } else if (currentNode.next != null) {
+                currentNode = currentNode.next;
+            } else {
+                containsValue = false;
+            }
+        }
+
+        // If the value is not in the list, return false and let the user know.
+        System.out.println("The linked list contains the value '" + value + "': " + containsValue + "\n");
+        return containsValue;
+    }
+
 
     // ---------------------------------------------------------------------------------
     // 9. find(value) returns the index of the node containing value, or nil if not found.
-    // TODO
+    public Integer find(Integer value) {
+
+        // Set the variables to hold the index and current node to the beginning of the linked list.
+        Integer index = 0;
+        Node currentNode = head;
+        int listSize = size();
+
+        if (head == null) { // Check if the linked list is empty.
+            System.out.println("The linked list is empty. There is nothing to find. :(");
+        } else {
+            // Iterate through the linked list and compare each node value to the requested value.
+            for (int i = 0; i < listSize; i++) {
+                if (currentNode.value == value) {
+                    System.out.println("Success! The value '" + value + "' was found at index " + index + ".");
+                    break;
+                } else if (currentNode.next != null) {
+                    currentNode = currentNode.next;
+                    index++;
+                } else {
+                    System.out.println("The value '" + value + "' was not found.");
+                    return null;
+                }
+            }
+        }
+        return index;
+    }
+
 
     // ---------------------------------------------------------------------------------
     // BONUS: printLL() prints the LinkedList that called it.
@@ -151,7 +203,6 @@ public class LinkedList {
     public void printLL() {
 
         Node current = head; // Starting at the head, set a node to hold the current value to be printed.
-//        System.out.print("Linked List: ");
 
         while (current != null) { // Traverse through the LinkedList.
             System.out.print(current.value + " --> "); // Print the value of the current node.
